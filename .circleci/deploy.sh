9c5672b7dc27dde5bb81c1a802b31f9906668697
beratiyilik <beratiyilik@hotmail.com>
2022-09-11 19:59:57 +0100

    deploy test 12

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 echo "EXECUTING deploy.sh FILE!";
 
-rsync -e "ssh -o StrictHostKeyChecking=no" -va --delete ./ root@ssh.beratiyilik.com:/var/www/dev.beratiyilik.com --exclude={".git",".gitignore","node_modules",".circleci","deploy.sh"}  # deploy the project
+rsync -e "ssh -o StrictHostKeyChecking=no" -va --delete ./ $SERVER_USERNAME@$SERVER_HOSTNAME:/var/www/dev.beratiyilik.com --exclude={".git",".gitignore","node_modules",".circleci","deploy.sh"}  # deploy the project
 
 ssh -oStrictHostKeyChecking=no -v $SERVER_USERNAME@$SERVER_HOSTNAME<<EOF
     cd /var/www/dev.beratiyilik.com;
