132ec48fbe0af0b10042f51c2cccdd5c8d5a3f47
beratiyilik <beratiyilik@hotmail.com>
2022-09-11 22:29:31 +0100

    deploy test 26

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -5,21 +5,17 @@ echo "Working directory: $PWD";
 
 echo "ls -la: $(ls -la)";
 
-#ssh -oStrictHostKeyChecking=no -v $SERVER_USERNAME@$SERVER_HOSTNAME "cd /var/www/dev.beratiyilik.com && rm -rf build node_modules"
+rsync -e "ssh -oStrictHostKeyChecking=no -v" -va --delete ./ $SERVER_USERNAME@$SERVER_HOSTNAME:/var/www/dev.beratiyilik.com --exclude '.circleci' --exclude '.git' --exclude '.gitignore' --exclude 'build'
 
-#rsync -e "ssh -oStrictHostKeyChecking=no -v" -va --delete ./ $SERVER_USERNAME@$SERVER_HOSTNAME:/var/www/dev.beratiyilik.com --exclude '.circleci' --exclude '.git' --exclude '.gitignore' --exclude 'build' --exclude 'node_modules'
-
-#ssh -oStrictHostKeyChecking=no -v $SERVER_USERNAME@$SERVER_HOSTNAME<<EOF
-#    cd /var/www/dev.beratiyilik.com;
-#    npm install && npm run build;
-#    pm2 restart APP_TEST && systemctl restart nginx;
-#    pm2 show APP_TEST;
-#    systemctl status nginx;
-#    logout;
-#EOF
-
-
-echo "ls -la project: $(ls -la project)";
+ssh -oStrictHostKeyChecking=no -v $SERVER_USERNAME@$SERVER_HOSTNAME<<EOF
+    cd /var/www/dev.beratiyilik.com;
+    rm -rf build node_modules;
+    npm install && npm run build;
+    pm2 restart APP_TEST && systemctl restart nginx;
+    pm2 show APP_TEST;
+    systemctl status nginx;
+    logout;
+EOF
 
 
 echo "END OF deploy.sh FILE!";
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
   const error: Error = result.error;
   return (
     <div >
-      {`BIRDE BU ${data?.name}`}
+      {`HANIFE BIR ISIRAM MI? ${data?.name}`}
     </div>
   )
 }
