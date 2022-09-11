c898482bc24faeed7bc10e005cb43ce32b17dc7b
beratiyilik <beratiyilik@hotmail.com>
2022-09-11 19:51:37 +0100

    deploy test 11

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/zsh
-echo "Executing deploy.sh"
+echo "EXECUTING deploy.sh FILE!";
+
+rsync -e "ssh -o StrictHostKeyChecking=no" -va --delete ./ root@ssh.beratiyilik.com:/var/www/dev.beratiyilik.com --exclude={".git",".gitignore","node_modules",".circleci","deploy.sh"}  # deploy the project
 
 ssh -oStrictHostKeyChecking=no -v $SERVER_USERNAME@$SERVER_HOSTNAME<<EOF
-    pwd;
-    whoami;
+    cd /var/www/dev.beratiyilik.com;
+    npm install && npm run build;
+    pm2 restart APP_TEST;
+    systemctl restart nginx;
+    pm2 show APP_TEST;
+    systemctl status nginx;
     logout;
 EOF
+
+echo "END OF deploy.sh FILE!";
+
+## eof
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+# emulate -LR zsh # reset zsh options
+# export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+echo "DEPLOY HAS STARTED!";
+
+echo "LOCAL USER: $(whoami)" # print the user name
+
+echo "LOCAL WORKING DIRECTORY: $(pwd)" # print the current directory
+
+echo "LOCAL PATH: ${PATH}" # print the path
+
+rsync -va --delete ./ root@ssh.beratiyilik.com:/var/www/dev.beratiyilik.com --exclude={".git",".gitignore","node_modules",".circleci","deploy.sh"}  # deploy the project
+
+ssh root@ssh.beratiyilik.com<<EOF
+    cd /var/www/dev.beratiyilik.com;
+    npm install && npm run build;
+    pm2 restart APP_TEST;
+    systemctl restart nginx;
+    pm2 show APP_TEST;
+    systemctl status nginx;
+    logout;
+EOF
+
+curl -I https://dev.beratiyilik.com/
+
+echo "DEPLOY HAS FINISHED!";
+
+## eof
