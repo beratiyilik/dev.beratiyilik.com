6f77645b9c973ad37dd98d34c9d1d9bae690eeae
beratiyilik <beratiyilik@hotmail.com>
2022-09-12 17:09:54 +0100

    deploy test 42

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
   const error: Error = result.error;
   return (
     <div >
-      {`TEST FOR APP_USER YEMEK:  ${data?.name}`}
+      {`FINAL:  ${data?.name}`}
     </div>
   )
 }
