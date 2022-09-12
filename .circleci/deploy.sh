3030db505df58a86776e3e113d8518249d521c79
beratiyilik <beratiyilik@hotmail.com>
2022-09-13 00:13:50 +0100

    deploy test 71

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+
 echo "EXECUTING deploy.sh FILE!";
 
 echo "Working directory: $PWD";
 
-# echo "ls -la: $(ls -la)";
-
-echo "ENVIRONMENT VARIABLES:";
-printenv;
+echo "ls -la: $(ls -la)";
 
 rsync -e "ssh -oStrictHostKeyChecking=no -v" -va --delete ./ $VPS_USERNAME@$VPS_HOSTNAME:/var/www/dev.beratiyilik.com --exclude '.circleci' --exclude '.git' --exclude '.gitignore' --exclude 'build' --exclude 'node_modules'
 
@@ -19,7 +17,6 @@ ssh -oStrictHostKeyChecking=no -v $VPS_USERNAME@$VPS_HOSTNAME<<EOF
     systemctl status nginx;
     logout;
 EOF
-
 
 echo "END OF deploy.sh FILE!";
 
